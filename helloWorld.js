a8c4c4a3c7a6b8fffdf1fd136a75e1848c3ee97d
Jamie Yang <jamie@urad.com.tw>
2015-06-22 11:21:24 +0800

    fix output error

--- helloWorld.js
+++ helloWorld.js
@@ -3,15 +3,15 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res){
-    console.log('Header:' + req.headers);
+    console.log('Header:', req.headers);
     var body = '';
     req.on('data', function (chunk) {
         body += chunk;
         console.log('Partial Body:'+ body);
     });
     req.on('end', function(){
-        console.log('Body:' + body);
+        console.log('Body:', body);
     });    
     res.writeHead(200);
     res.end('hello nodejs server');
-}).listen(3000);
+}).listen(8080);
